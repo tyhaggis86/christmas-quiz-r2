eb0b2d5730e4ca2e1a9ed6919a1217056384a76b
tyhaggis86 <113810488+tyhaggis86@users.noreply.github.com>
2022-12-11 15:08:25 +1000

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,167 @@
+let questions = [
+    {
+    numb: 1,
+    question: "In the song Twelve Days of Christmas, what is given on the seventh day? ",
+    answer: "Swans a swimming",
+    options: [
+      "Gold rings",
+      "Turtle Doves",
+      "Swans a swimming",
+      "Lords a leaping"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which country came up with the dessert White Christmas made from coconut oil and dried fruit?",
+    answer: "Australia",
+    options: [
+      "Australia",
+      "New Zealand",
+      "South Africa",
+      "Fiji"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who holds the Guinness World Record for the Worlds largest gingerbread man?",
+    answer: "Ikea",
+    options: [
+      "Harrods",
+      "Ikea",
+      "Woolworths",
+      "Wal-Mart"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When was the first Christmas tree lit up in the Rockefeller Center?",
+    answer: "1933",
+    options: [
+      "1933",
+      "1945",
+      "1956",
+      "1963"
+    ]
+  },
+    {
+    numb: 5,
+    question: "In The Santa Clause, why does Scott take over the role of Santa Claus?",
+    answer: "Santa falls off his roof",
+    options: [
+      "He signed a contract",
+      "Santa falls off his roof",
+      "He promised Charlie ",
+      "It was his childhood dream"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which country is known to have a tradition of a witch dropping gifts for children through the chimney at Christmas?",
+    answer: "Italy",
+    options: [
+      "Iceland",
+      "Scotland",
+      "Germany",
+      "Italy"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the name of Scrooge’s dead business partner?",
+    answer: "Jacob Marley",
+    options: [
+      "Jacob Marley",
+      "Tiny Tim",
+      "Bob Marely",
+      "Bob Cratchit"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How many candles should go on an advent wreath?",
+    answer: "4",
+    options: [
+      "2",
+      "4",
+      "6",
+      "8"
+    ]
+  },
+  {
+    numb: 9,
+    question: "One of the most popular Christmas toys of 1971, who or what, ‘wobble but don’t fall down’?",
+    answer: "Weebles",
+    options: [
+      "Pet Rock",
+      "Stretch Armstrong",
+      "Weebles",
+      "Malibu Barbie"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which UK monarch delivered the first Christmas Broadcast to the BBC in 1932",
+    answer: "King George V",
+    options: [
+      "King George V",
+      "King Edward VIII",
+      "King George VI",
+      "Queen Elizabeth II"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which country is the largest exporter of Christmas trees?",
+    answer: "Canada",
+    options: [
+      "Norway",
+      "Finland",
+      "USA",
+      "Canada"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which US president banned Christmas trees from the White House?",
+    answer: "Theodore Roosevelt",
+    options: [
+      "William Howard Taft",
+      "Theodore Roosevelt",
+      "Woodrow Wilson",
+      "Warren G Harding"
+    ]
+  },
+  {
+    numb: 13,
+    question: "How many reindeer are there including Rudolph?",
+    answer: "9",
+    options: [
+      "7",
+      "9",
+      "11",
+      "13"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which fairy tale served as an inspiration for the first gingerbread houses?",
+    answer: "Hansel and Gretel",
+    options: [
+      "Hansel and Gretel",
+      "Snow White",
+      "Sleeping Beauty",
+      "The Snow Queen"
+    ]
+  },
+  {
+    numb: 15,
+    question: "What country has the tradition of filling children's clogs with treats and candies on December 5th?",
+    answer: "The Netherlands",
+    options: [
+      "Germany",
+      "Poland",
+      "Belgium",
+      "The Netherlands"
+    ]
+  },
+];
